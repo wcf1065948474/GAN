f9ae8df353b964295f8c29e77012bb697acb86c7
thinker <thinker1065948474@gmail.com>
2019-07-02 12:42:59 +0800

    fixbug:load_vgg16()

--- model.py
+++ model.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 from torch.autograd import Variable
 from batchnorm import SynchronizedBatchNorm2d as SynBN2d
 from dataset import pad_tensor,pad_tensor_back
-from torch.utils.serialization import load_lua
 from collections import OrderedDict
 
 
@@ -397,21 +396,13 @@ class Vgg16(nn.Module):
 
 def load_vgg16(model_dir):
     """ Use the model from https://github.com/abhiskk/fast-neural-style/blob/master/neural_style/utils.py """
-    if not os.path.exists(model_dir):
-        os.mkdir(model_dir)
-    if not os.path.exists(os.path.join(model_dir, 'vgg16.weight')):
-        if not os.path.exists(os.path.join(model_dir, 'vgg16.t7')):
-            os.system('wget https://www.dropbox.com/s/76l3rt4kyi3s8x7/vgg16.t7?dl=1 -O ' + os.path.join(model_dir, 'vgg16.t7'))
-        vgglua = load_lua(os.path.join(model_dir, 'vgg16.t7'))
+    if not os.path.exists(os.path.join(model_dir,'vgg16.weight')):
+        print("vgg16.weight not find")
+        vgg = None
+    else:
         vgg = Vgg16()
-        for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
-            dst.data[:] = src
-        torch.save(vgg.state_dict(), os.path.join(model_dir, 'vgg16.weight'))
-    vgg = Vgg16()
-    vgg.cuda()
-    #vgg.cuda(device=gpu_ids[0])
-    vgg.load_state_dict(torch.load(os.path.join(model_dir, 'vgg16.weight')))
-    #vgg = torch.nn.DataParallel(vgg, gpu_ids)
+        vgg.cuda()
+        vgg.load_state_dict(torch.load(os.path.join(model_dir, 'vgg16.weight')))
     return vgg
 
 
